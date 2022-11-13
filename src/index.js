f6c41ed65fb7748c43ae37d93c4b7863452e357e
subhamgoto <subham.bubai92@gmail.com>
2022-11-13 12:41:08 +0530

    initial change

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@
  * @fileoverview Adds blocks that replace the built-in mutator UI with a +/- UI.
  */
 
-import './if.js';
-import './list_create.js';
-import './procedures.js';
-import './text_join';
+// import './if.js';
+// import './list_create.js';
+// import './procedures.js';
+// import './text_join';
+import './make_lookup';
